9c8eeb2f8634638b6362fd8201f64686986b62a5
Stajila <2835318860@qq.com>
2023-12-10 23:21:47 -0600

    Update download.sh
    syntax

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 index=$1
 torrent_file=$2
-aria2c --file-allocation=none --seed-time=0 --select-file=$index $torrent_file
+aria2c --file-allocation=none --seed-time=0 --select-file="$index" "$torrent_file"
